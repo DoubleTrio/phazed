877e140dd85c0f264bbe2cb683318193fc0d553c
Nifiore <nicholaszfiore@gmail.com>
2024-10-06 22:51:46 -0400

    Rearrange files

---
+++ Scripts/character.gd
@@ -0,0 +1,41 @@
+extends Area2D
+
+var animation_speed = 5
+var direction = 0
+var moving = false
+var grid_size = 16
+var inputs = {
+	"right": Vector2.RIGHT,
+	"left": Vector2.LEFT,
+	"up": Vector2.UP,
+	"down": Vector2.DOWN
+}
+
+
+#var direction = 0
+#func _ready():
+	#position = position.snapped(Vector2.ONE * tile_size)
+	#position += Vector2.ONE * tile_size/2 hahahehe
+	#meow mwow
+# meow meow
+# TODO Change to not raycast?
+func _unhandled_input(event):
+	if moving:
+		return
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+
+			
+@onready var ray = $RayCast2D
+func move(dir):
+	ray.target_position = inputs[dir] * grid_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		#position += inputs[dir] * tile_size
+		var tween = create_tween()
+		tween.tween_property(self, "position",
+			position + inputs[dir] * grid_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		moving = true
+		await tween.finished
+		moving = false
