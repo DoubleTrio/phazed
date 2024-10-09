0e629994566d6ed8773b8322cd3de02d8ba28456
Kace <lakacey03@gmail.com>
2024-10-08 20:51:39 -0400

    character flips direction and sprite

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,41 +1,57 @@
 extends Area2D
 
-var animation_speed = 5
-var direction = 0
+var animation_speed = 16
 var moving = false
 var grid_size = 16
+
+var dir_string = "right"
+@onready var layer = $"../TileMapLayer"
+
 var inputs = {
 	"right": Vector2.RIGHT,
-	"left": Vector2.LEFT,
-	"up": Vector2.UP,
-	"down": Vector2.DOWN
+	"left": Vector2.LEFT
 }
 
+@onready var timer = $Timer
+@onready var sprite = $AnimatedSprite2D
+@onready var ray = $RayCast2D
 
-#var direction = 0
 #func _ready():
 	#position = position.snapped(Vector2.ONE * tile_size)
-	#position += Vector2.ONE * tile_size/2 hahahehe
-	#meow mwow
-# meow meow
-# TODO Change to not raycast?
+	
 func _unhandled_input(event):
-	if moving:
-		return
-	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
-			move(dir)
+	if event.is_action_pressed(dir_string):
+		move(dir_string)
+		if timer.is_stopped():
+			timer.start()
 
-			
-@onready var ray = $RayCast2D
+func _on_timer_timeout():
+	move(dir_string)
+	
+	# TODO: LATER - RESTART THE TIMER AFTER ALL EVENTS INVOLVED WITH 
+	# THE PLATER ARE FINISHED like falling
+	timer.start()
+	
+# TODO - This code is really unclean, clean up later 
+# once we have a better idea of how we want the player to move
+# We will need to account for the player falling down
 func move(dir):
 	ray.target_position = inputs[dir] * grid_size
 	ray.force_raycast_update()
 	if !ray.is_colliding():
-		#position += inputs[dir] * tile_size
-		var tween = create_tween()
-		tween.tween_property(self, "position",
-			position + inputs[dir] * grid_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
-		moving = true
-		await tween.finished
-		moving = false
+		pass
+	else:
+		if dir_string == "left":
+			dir_string = "right"
+			dir = "right"
+			sprite.flip_h = false
+		else: 
+			dir_string = "left"
+			dir = "left"
+			sprite.flip_h = true
+	var tween = create_tween()
+	tween.tween_property(self, "position", position + inputs[dir] *  grid_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+	moving = true
+	sprite.play("walk", 2)
+	await tween.finished
+	moving = false
