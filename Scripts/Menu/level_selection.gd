fe3a34e85a9debf30be47802d64b3e0fca1679a8
BonelessPi <84295882+BonelessPi@users.noreply.github.com>
2024-11-11 17:30:32 -0500

    Improve layout of level select

--- Scripts/Menu/level_selection.gd
+++ Scripts/Menu/level_selection.gd
@@ -14,13 +14,15 @@ func get_levels(path) -> void:
 	
 	var files_list = Array(dir.get_files())
 	files_list.sort_custom(func	(a:String,b): return float(a.split("_",true,1)[1])<float(b.split("_",true,1)[1]))
-#	
+	var min_width = files_list.map(func (s): return s.length()).max() * 15
+	var min_size = Vector2(min_width,50)
 	for file_name: String in files_list:
 		print(file_name)
-		create_level_btn('%s/%s' % [dir.get_current_dir(),file_name], file_name)
+		create_level_btn('%s/%s' % [dir.get_current_dir(),file_name], file_name, min_size)
 
-func create_level_btn(lvl_path:String, file_name:String) -> void:
+func create_level_btn(lvl_path:String, file_name:String, min_size:Vector2) -> void:
 	var btn = LEVEL_BTN.instantiate()
 	btn.text = "Level "+file_name.split("_",true,1)[1].trim_suffix(".tscn")
 	btn.level_path = lvl_path
+	btn.set_custom_minimum_size(min_size)
 	grid.add_child(btn)
