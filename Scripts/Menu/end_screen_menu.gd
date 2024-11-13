3b77cfcc18d5204d0430827ee73e88057e6b22a8
BonelessPi <84295882+BonelessPi@users.noreply.github.com>
2024-11-13 00:06:49 -0500

    Fix next button on last level
    Remove helpers.gd

--- Scripts/Menu/end_screen_menu.gd
+++ Scripts/Menu/end_screen_menu.gd
@@ -2,18 +2,25 @@ extends Control
 
 class_name EndScreenMenu
 
+var next_lvl_file_path
 @onready var teleport_info = $VBoxContainer/TeleportInfo
+@onready var next_button = $VBoxContainer/HBoxContainer/MarginContainer2/Next
 
-func _on_next_button_down():
+func _ready() -> void:
 	var file_path = get_tree().current_scene.scene_file_path
 	var regex = RegEx.new()
 	regex.compile("^res://Scenes/Levels/level_(\\d+)\\.tscn$")
 	var curr_lvl_num = int(regex.search(file_path).get_string(1))
 	regex.compile("\\d+(?=\\.tscn)")
-	var next_lvl_file_path = regex.sub(file_path,str(curr_lvl_num+1))
-	print(next_lvl_file_path)
+	self.next_lvl_file_path = regex.sub(file_path,str(curr_lvl_num+1))
+	if !FileAccess.file_exists(self.next_lvl_file_path):
+		self.next_lvl_file_path = null
+		next_button.disabled = true
+
+func _on_next_button_down():
+	print("next level button triggered")
 	LevelScene.instance = null
-	get_tree().change_scene_to_file.bind(next_lvl_file_path).call_deferred()
+	get_tree().change_scene_to_file.bind(self.next_lvl_file_path).call_deferred()
 
 func _on_level_selector_button_down():
 	get_tree().change_scene_to_file("res://Scenes/Menu/level_selection.tscn")
